757c690a3d862e3031fceb8476d8624801bdfb81
renattaschunemann <renattaschunemann@hotmail.com>
2026-05-16 11:54:49 -0300

    First commit

---
+++ Fintech-FinSeven/src/main/java/br/com/fiap/fintech/finseven/FintechFinSevenApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fintech.finseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechFinSevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechFinSevenApplication.class, args);
+	}
+
+}
---
+++ Fintech-FinSeven/src/test/java/br/com/fiap/fintech/finseven/FintechFinSevenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fintech.finseven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechFinSevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
